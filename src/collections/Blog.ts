45a493c8899122386d59eaec85bb5617fc379c38
Arthur Moyo <moyoarthur22@gmail.com>
2025-10-12 11:35:07 +0200

    Add Blog collection configuration with title and content fields

---
+++ src/collections/Blog.ts
@@ -0,0 +1,23 @@
+import type { CollectionConfig } from 'payload'
+
+export const Blog: CollectionConfig = {
+  slug: 'blog',
+  admin: {
+    useAsTitle: 'title',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'richText',
+      required: true,
+    },
+  ],
+}
